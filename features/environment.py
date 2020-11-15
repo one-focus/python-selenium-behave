3803334ffdcbf55fb520f3309cf450353db82c33
sozdai <sash.kardash@gmail.com>
2020-11-15 07:31:36 +0300

    added headless

--- features/environment.py
+++ features/environment.py
@@ -1,10 +1,13 @@
 import configparser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 # TODO check all context attributes on https://behave.readthedocs.io/en/latest/context_attributes.html#user-attributes
 def before_all(context):
-    context.driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    context.driver = webdriver.Chrome(options=chrome_options)
     context.driver.implicitly_wait(5)
     # read config
     parser = configparser.ConfigParser()
