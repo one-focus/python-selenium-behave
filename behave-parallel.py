44a4c4b74634b038e722b7adb7ea44a6108f5f06
sozdai <sash.kardash@gmail.com>
2020-11-16 12:32:01 +0300

    updated run config with --no-summary

--- behave-parallel.py
+++ behave-parallel.py
@@ -59,7 +59,7 @@ def main():
     args = parse_arguments()
     pool = Pool(args.processes)
     if args.tags:
-        p = Popen('behave -d -f json --no-skipped -t {}'.format(args.tags),
+        p = Popen('behave -d -f json --no-summary --no-skipped -t {}'.format(args.tags),
                   stdout=PIPE, shell=True)
         out, err = p.communicate()
         j = json.loads(out.decode())
