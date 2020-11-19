0286482a9fdf352bb9904a3c972f2f0b5a85b6f9
sozdai <sash.kardash@gmail.com>
2020-11-19 11:46:58 +0300

    update actions

--- features/environment.py
+++ features/environment.py
@@ -7,9 +7,21 @@ from selenium.webdriver.chrome.options import Options
 
 # TODO check all context attributes on https://behave.readthedocs.io/en/latest/context_attributes.html#user-attributes
 def before_all(context):
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    context.driver = webdriver.Chrome(options=chrome_options)
+    # chrome_options = Options()
+    # chrome_options.add_argument('--headless')
+    # context.driver = webdriver.Chrome(options=chrome_options)
+
+    capabilities = {
+        'browserName': 'chrome',
+        'version': '86.0',
+        'enableVNC': True,
+        'enableVideo': True
+    }
+
+    context.driver = webdriver.Remote(
+        command_executor='http://159.65.133.63:4444/wd/hub',
+        desired_capabilities=capabilities)
+
     context.driver.implicitly_wait(5)
     # read config
     parser = configparser.ConfigParser()
